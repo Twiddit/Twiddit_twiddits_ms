61c472a83f4dfa4ab194865e848d12ce6bf60dd4
Nrinconv <nrinconv@unal.edu.co>
2023-06-19 02:15:32 -0500

    test 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { InfoTwidditModule } from './info-twiddit/info-twiddit.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb://twiddit:12345@10.0.1.6:27017'),
+    MongooseModule.forRoot('mongodb://twiddit:12345@10.0.1.6:27017/?directConnection=true'),
     TwidditModule,
     LikeModule,
     DislikeModule,
