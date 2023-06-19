070ba4d6ae7220b0f92c5923adf06ddbd3669ea4
Nrinconv <nrinconv@unal.edu.co>
2023-06-18 21:34:16 -0500

    Added admin to db

--- dist/app.module.js
+++ dist/app.module.js
@@ -25,7 +25,7 @@ AppModule = __decorate([
             config_1.ConfigModule.forRoot({
                 isGlobal: true,
             }),
-            mongoose_1.MongooseModule.forRoot('mongodb://twiddit:12345@34.81.212.38:27017/twiddits?authSource=admin'),
+            mongoose_1.MongooseModule.forRoot('mongodb://twiddit:12345@34.81.212.38:27017?authSource=admin'),
             twiddit_module_1.TwidditModule,
             like_module_1.LikeModule,
             dislike_module_1.DislikeModule,
