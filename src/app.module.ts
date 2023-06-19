48cc19da299a49087b5fd85bc6439b522ce25646
Nrinconv <nrinconv@unal.edu.co>
2023-06-18 21:15:03 -0500

    Url db changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { InfoTwidditModule } from './info-twiddit/info-twiddit.module';
       isGlobal: true,
     }),
     MongooseModule.forRoot(
-      'mongodb://twiddit:12345@34.81.212.38:27017/twiddits?authSource=admin',
+      'mongodb://twiddit:12345@34.81.212.38:27017',
     ),
     TwidditModule,
     LikeModule,
